13821314e1545134e0a4b1ccdfd48a9c30848f6f
hasanasadov <hasanaliasadov@gmail.com>
2024-10-11 01:32:25 +0400

    base url changed to custom deploy url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://hasagram-server.vercel.app";
 export const MODAL_TYPE = {
-  CREATE: 'CREATE',
-  EDIT: 'EDIT',
+  CREATE: "CREATE",
+  EDIT: "EDIT",
 };
