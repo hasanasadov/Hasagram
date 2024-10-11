35849411632eb2fa4132bc8d0befac3285407a3c
hasanasadov <hasanaliasadov@gmail.com>
2024-10-12 01:14:11 +0400

    server

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://hasagram-server.vercel.app/";
+export const BASE_URL = "https://hasagram-server.vercel.app";
 export const MODAL_TYPE = {
   CREATE: 'CREATE',
   EDIT: 'EDIT',
